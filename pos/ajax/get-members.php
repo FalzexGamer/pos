cc1fc708fd0b107271e8b692f05ade4d49f88499
FalzexProgrammer <muhammadfirdausbinmohdazamli@gmail.com>
2025-08-27 01:21:53 +0800

    Delete pos directory

--- pos/ajax/get-members.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../include/conn.php';
-
-$query = mysqli_query($conn, "SELECT * FROM members WHERE is_active = 1 ORDER BY name");
-
-while ($member = mysqli_fetch_array($query)) {
-    echo '<option value="' . $member['id'] . '">' . $member['name'] . ' (' . $member['member_code'] . ')</option>';
-}
-?>
--- pos/include/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'pos_system');
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
